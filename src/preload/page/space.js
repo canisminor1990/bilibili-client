7dfbcf4433f04ac9d58a7b7301727b49f78dc476
Zac Yu <me@zacyu.com>
2018-01-03 18:39:18 -0800

    Use tab for wrapped style rules.

--- src/preload/page/space.js
+++ src/preload/page/space.js
@@ -25,7 +25,7 @@ export function style() {
   AddStyle(`
 		.index__banner__M-space-banner- img,
 		.index__exp__M-space-info-,
-        .index__call__M-space-info- {
+		.index__call__M-space-info- {
 			display: none !important;
 		}
 		.index__personal__M-space-personal- {
@@ -40,9 +40,9 @@ export function style() {
 			border: 3px solid #fff;
 			box-shadow: 0 4px 8px rgba(0, 0, 0, .1);
 		}
-        .index__big__M-space-info- {
-		    left: 9rem !important;
-		    top: -1.8rem !important;
+		.index__big__M-space-info- {
+			left: 9rem !important;
+			top: -1.8rem !important;
 		}
 		.index__divider__M-space- {
 			background: #f25d8e;
